b5f649de39f0b470e01009f5000aabc101bd8bc9
kimhanil-dev <khlj752@naver.com>
2024-01-26 22:51:09 +0900

    project init

---
+++ Source/TwoSomePlace/TwoSomePlace.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TwoSomePlace.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwoSomePlace, "TwoSomePlace" );
