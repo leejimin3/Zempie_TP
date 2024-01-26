cfa52b2ef1b5f9441cbbfc3d3987aa27160f221a
kimhanil-dev <khlj752@naver.com>
2024-01-27 07:04:02 +0900

    [Add] MakeChildAnimal

--- Source/TwoSomePlace/Private/AnimalFarmGameMode.cpp
+++ Source/TwoSomePlace/Private/AnimalFarmGameMode.cpp
@@ -3,8 +3,10 @@
 
 #include "AnimalFarmGameMode.h"
 #include "../Parent_Animal.h"
+#include "../Child_Animal.h"
 
 #include "Kismet/KismetMathLibrary.h"
+
 
 void AAnimalFarmGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
 {
@@ -14,7 +16,7 @@ void AAnimalFarmGameMode::InitGame(const FString& MapName, const FString& Option
 
 void AAnimalFarmGameMode::SpawnAnimals(int animalSpawnCount)
 {
-	check(GetWorld() != nullptr);
+	//check(GetWorld() != nullptr);
 
 	// upper player		
 	for (int i = 0; i < animalSpawnCount; ++i)
@@ -35,4 +37,49 @@ void AAnimalFarmGameMode::SpawnAnimals(int animalSpawnCount)
 		animal->Set_LeftDownFarmAria(FarmLeftDown);
 		animal->Set_RightUpFarmAria(FarmRightUp);
 	}
+}
+
+AChild_Animal* AAnimalFarmGameMode::MakeChildAnimal(const AParent_Animal* parent1, const AParent_Animal* parent2)
+{
+	AChild_Animal* childAnimal = nullptr;
+	childAnimal = NewObject<AChild_Animal>();
+
+	// 반복문으로 일괄 처리하기 위한 처리
+	const TArray<int32>* parentsPartsInfo[4][2] =
+	{
+		{&parent1->head, &parent2->head},
+		{&parent1->body, &parent2->body},
+		{&parent1->leg, &parent2->leg},
+		{&parent1->tail, &parent2->tail},
+	};
+
+	TArray<int32>* childPartsInfo[4] =
+	{
+		&childAnimal->head,
+		&childAnimal->body,
+		&childAnimal->leg,
+		&childAnimal->tail
+	};
+
+	// parent1, parent2의 특성을 random하게 child에게 물려주는 로직
+	unsigned int selector = 0;
+	for (int i = 0; i < 4; ++i)
+	{
+		selector = UKismetMathLibrary::RandomInteger(1);
+		const TArray<int32>* selectedPartsInfo = parentsPartsInfo[i][selector];
+
+		childPartsInfo[i]->SetNum(selectedPartsInfo->Num());
+
+		for (int j = 0; j < selectedPartsInfo->Num(); ++j)
+		{
+			(*childPartsInfo[i])[j] = (*selectedPartsInfo)[j];
+		}
+	}
+
+	return childAnimal;
+}
+
+void AAnimalFarmGameMode::LoadAnimalDatas()
+{
+	
 }
--- Source/TwoSomePlace/Public/AnimalFarmGameMode.h
+++ Source/TwoSomePlace/Public/AnimalFarmGameMode.h
@@ -7,11 +7,37 @@
 #include "GameFramework/GameMode.h"
 #include "AnimalFarmGameMode.generated.h"
 
+USTRUCT(BlueprintType)
+struct TWOSOMEPLACE_API FAnimalData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimalState")
+	FName animalName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimalState")
+	TArray<FName> animalTypes;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimalState")
+	TArray<float> head;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimalState")
+	TArray<float> body;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimalState")
+	TArray<float> leg;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimalState")
+	TArray<float> tail;
+};
 
 /**
  * 
  */
 class AParent_Animal;
+class AChild_Animal;
 
 UCLASS()
 class TWOSOMEPLACE_API AAnimalFarmGameMode : public AGameMode
@@ -23,6 +49,9 @@ public:
 	UFUNCTION()
 	virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
 public:
+
+	UPROPERTY(EditAnywhere, Category = "AnimalState")
+	UDataTable* AnimalDataTable;
 
 	UPROPERTY(EditAnywhere, Category = "Spawn")
 	FVector SpawnOrigin;
@@ -39,5 +68,15 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Spawn")
 	void SpawnAnimals(int animalSpawnCount);
 
+	UFUNCTION(BlueprintCallable, Category = "InGame")
+	AChild_Animal* MakeChildAnimal(const AParent_Animal* parent1, const AParent_Animal* parent2);
+
+	UPROPERTY(BlueprintReadWrite, Category = "Animal")
 	TArray<AParent_Animal*> Animals;
+
+protected:
+
+	TMap<FName, FAnimalData> AnimalDatas;
+
+	void LoadAnimalDatas();
 };
